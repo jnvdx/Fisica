cd8e59b36ef28843418b5fb9b1a5417298944965
jnvdx <44271280+jnvdx@users.noreply.github.com>
2019-03-18 18:47:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import math
+
+
+def calculadora_segundo_grado(a,b,c):
+
+    raiz =  math.sqrt( b**2 - (4 * a * c))
+    x = (-b + raiz ) / (2 * a)
+    y = (-b - raiz ) / (2 * a)
+
+    print("Solucion 1: {}".format(x))
+    print("Solucion 2: {}".format(y))
+
+def menu():
+
+    print("Seleccion una opcion >>")
+    print("\t 1) Incognita del espacio (x)")
+    print("\t 2) Incognita del tiempo (x)")
+    print("\t 3) Incognita del aceleracion (x)")
+
+
+menu()
+
+eleccion = int(input("Introduce una opcion >>"))
+
+lista = [1, 2, 3,]
+
+while eleccion in lista:
+
+    if eleccion == 1:
+
+        tiempo = float(input("Introduce el tiempo (s) >>"))
+        espacio_incial = float(input("Introduce el espacio inicia l>>"))
+        velocidad_incial = float(input("Introduce la velocidad inicial >>"))
+        aceleracion =float(input("Introduce la aceleracion >>"))
+
+        espacio = espacio_incial + (velocidad_incial * tiempo) - ((aceleracion * (tiempo**2)) / 2)
+
+        print("Solucion: {}".format(tiempo))
+
+    elif eleccion == 2:
+
+        espacio_final = float(input("Introduce el espacio final (x) >>"))
+        espacio_incial = float(input("Introduce el espacio inicia l>>"))
+        velocidad_incial = float(input("Introduce la velocidad inicial >>"))
+        aceleracion =float(input("Introduce la aceleracion >>"))
+
+        a = aceleracion
+        b = velocidad_incial
+        c = espacio_incial - espacio_final
+
+        calculadora_segundo_grado(a,b,c)
+
+    
+
+
+
+
+
+print(">>>>> ERROR <<<<<")
