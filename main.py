c8d850ede12cb7930b61611c0e085674bda1410b
jnvdx <jon.agramont15@gmail.com>
2019-03-19 14:52:29 +0100

    He resuelto los errores.

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from sympy import *
 import math
 
 
+def calculadora_segundo_grado(a, b, c):
 
-def calculadora_segundo_grado(a,b,c):
-
-    raiz =  math.sqrt( b**2 - (4 * a * c))
-    x = (-b + raiz ) / (2 * a)
-    y = (-b - raiz ) / (2 * a)
+    raiz = math.sqrt(b**2 - (4 * a * c))
+    x = (-b + raiz) / (2 * a)
+    y = (-b - raiz) / (2 * a)
 
     print("Solucion 1: {}".format(x))
     print("Solucion 2: {}".format(y))
+
 
 def menu():
 
@@ -20,7 +20,6 @@ def menu():
     print("\t 2) Incognita del tiempo (x)")
     print("\t 3) Incognita del aceleracion (x)")
     print("\t 4) Tiro parabolico")
-
 
 
 menu()
@@ -47,13 +46,13 @@ while eleccion in lista:
         espacio_final = float(input("Introduce el espacio final (x) >>"))
         espacio_incial = float(input("Introduce el espacio inicia l>>"))
         velocidad_incial = float(input("Introduce la velocidad inicial >>"))
-        aceleracion =float(input("Introduce la aceleracion >>"))
+        aceleracion = float(input("Introduce la aceleracion >>"))
 
         a = aceleracion
         b = velocidad_incial
         c = espacio_incial - espacio_final
 
-        calculadora_segundo_grado(a,b,c)
+        calculadora_segundo_grado(a, b, c)
 
 
     elif eleccion == 3:
@@ -62,27 +61,11 @@ while eleccion in lista:
         tiempo = float(input("Introduce el tiempo (s) >>"))
         espacio_incial = float(input("Introduce el espacio inicia l>>"))
         velocidad_incial = float(input("Introduce la velocidad inicial >>"))
+        celeracion = ((espacio_final - espacio_incial) - (velocidad_incial * tiempo) / (1/2 * (tiempo**2)))
 
-        aceleracion = (espacio_final - espacio_incial - (velocidad_incial * tiempo)) / (1/2 * (tiempo**2)
-
-        print("Solucion: {}".format(aceleracion))
+        print("Solucion:")
 
 
     elif eleccion == 4:
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-print(">>>>> ERROR <<<<<")
+        end;
