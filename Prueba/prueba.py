811e5276e4f771fb3f71a902c1089af669f6a001
jnvdx <jon.agramont15@gmail.com>
2019-03-19 20:58:26 +0100

    He terminado la funcion velocidad_descompuesta

--- Prueba/prueba.py
+++ Prueba/prueba.py
@@ -1,18 +1,29 @@
 
 import math
 from sympy import *
-def velocidad_descompuesta(v_0, B):
+
+
+
+def velocidad_descompuesta(v_0, B,):
 
     a = math.radians(B)
 
     v_0x = v_0 * math.cos(a)
     v_0y = v_0 * math.sin(a)
 
-    print(v_0x)
-    print(v_0y)
+    lista = [v_0x, v_0y]
+
+    return lista
+
 
 
 v_0 = float(input("Introducir velocidad inical >>"))
 B = float(input("Introduce el angulo de tiro >>"))
 
 velocidad_descompuesta(v_0,B)
+
+v_x = velocidad_descompuesta(v_0, B,)[0]
+v_y = velocidad_descompuesta(v_0, B,)[1]
+
+print(v_x)
+print(v_y)
