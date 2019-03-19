8023f49e73d4e5915baed80a54c2b8e498a09370
jnvdx <jon.agramont15@gmail.com>
2019-03-19 13:36:51 +0100

    Iniciando la contrucion de la funcion tiro psrsbolico

--- Prueba/prueba.py
+++ Prueba/prueba.py
@@ -1,8 +1,32 @@
 
+import math
 from sympy import *
-
+"""
 x = Symbol('x')
 
 y = solve(x**2 + 4*x -17)
 
 print(y)
+"""
+
+v_0 = float(input("Introducir velocidad inical >>"))
+a = float(input("INtroduce el angulo de tiro >>"))
+
+def velocidad_descompuesta(v_0,a):
+
+    v_0x = v_0 * math.cos(a)
+    v_0y = v_0 * math.sin(a)
+
+    return v_0x
+    return v_0y
+
+
+"""
+Calcular primero el punto mas alto
+"""
+
+velocidad_descompuesta(v_0,a)
+
+v_0x = velocidad_descompuesta(v_0)
+
+print(v_0x)
