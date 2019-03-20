e38c8017285b9be14a7bf92e433b67db625c8dac
jnvdx <jon.agramont15@gmail.com>
2019-03-20 07:32:23 +0100

    Terminada la base del eercicio

---
+++ Ejercicio1_preolimpiadas.py
@@ -0,0 +1,14 @@
+
+x = int(input("Introduce tu numero entero >>"))
+"""
+lista = list(range(1, (x + 1))
+
+for i in range(1, (x + 1):
+
+    y = 1 + i
+
+    print(y)
+"""
+
+for x in range(0, x):
+    print(1+(x))
